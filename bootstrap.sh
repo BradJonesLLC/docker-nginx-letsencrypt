c8d6ee3b93268da601583d67d01dd9efc24d733e
Brad Jones <brad@bradjonesllc.com>
2016-08-14 15:20:17 -0600

    Update bootstrap to correctly create the dhparam directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
-if [ ! -f /etc/ssl/dhparam/dhparam.pem ]; then cd /etc/ssl/certs && openssl dhparam -out dhparam.pem 2048; fi
+\
+if [ ! -f /etc/ssl/dhparam/dhparam.pem ]; then mkdir -p /etc/ssl/dhparam && cd /etc/ssl/dhparam && openssl dhparam -out dhparam.pem 2048; fi
 
 /certs.sh && supervisord -n
